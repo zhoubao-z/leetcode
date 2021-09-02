4134bdd22591ad62b0f938994ddf746ffe145e57
zhoubao <zhoubao_z@163.com>
2021-09-02 09:48:08 +0800

    commit 0902

---
+++ src/record_202109/_0902_getKthFromEnd.java
@@ -0,0 +1,32 @@
+package record_202109;
+/**
+ * 链表中倒数第k个节点
+ * https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+ */
+
+import dataStructure.ListNode;
+
+public class _0902_getKthFromEnd {
+    public static ListNode getKthFromEnd(ListNode head, int k) {
+        if(k<=0){
+            return new ListNode();
+        }
+        ListNode fast=head;
+        ListNode slow=head;
+        for(int i=0;i<k&&fast!=null;i++){
+            fast=fast.next;
+        }
+        while(fast!=null&&slow!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode node=new ListNode();
+        int[] nums=new int[]{1,2,3,4,5};
+       ListNode head= node.getLinkedList(nums);
+        System.out.println(getKthFromEnd(head,4).val);
+    }
+}
