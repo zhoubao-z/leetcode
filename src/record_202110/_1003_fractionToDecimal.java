e9d3baa350e85d70a64d53d27d4affc348a65e07
zhoubao <zhoubao_z@163.com>
2021-10-19 16:19:48 +0800

    commit 1019

---
+++ src/record_202110/_1003_fractionToDecimal.java
@@ -0,0 +1,11 @@
+package record_202110;
+
+public class _1003_fractionToDecimal {
+    public String fractionToDecimal(int numerator, int denominator) {
+
+        return "";
+    }
+    public static void main(String[] args) {
+        System.out.println(4f/333);
+    }
+}
