6e3c4928b0e1f02c4a731b54bedce53f94f9b487
zhoubao <zhoubao_z@163.com>
2021-10-20 16:13:47 +0800

    commit 1004

---
+++ src/record_202110/_1004_licenseKeyFormatting.java
@@ -0,0 +1,29 @@
+package record_202110;
+
+/**
+ * https://leetcode-cn.com/problems/license-key-formatting/
+ */
+public class _1004_licenseKeyFormatting {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder ans = new StringBuilder();
+        int cnt = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != '-') {
+                cnt++;
+                ans.append(Character.toUpperCase(s.charAt(i)));
+                if (cnt % k == 0) {
+                    ans.append("-");
+                }
+            }
+        }
+        if (ans.length() > 0 && ans.charAt(ans.length() - 1) == '-') {
+            ans.deleteCharAt(ans.length() - 1);
+        }
+
+        return ans.reverse().toString();
+    }
+    public static void main(String[] args) {
+
+    }
+}
