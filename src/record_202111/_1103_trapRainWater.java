5fc1a6141c1933dea96bbad8cbe83ab9550ca47c
zhoubao <zhoubao_z@163.com>
2021-11-03 19:12:04 +0800

    commit 1103

---
+++ src/record_202111/_1103_trapRainWater.java
@@ -0,0 +1,68 @@
+package record_202111;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode-cn.com/problems/trapping-rain-water-ii/
+ * 407. 接雨水 II
+ */
+public class _1103_trapRainWater {
+
+    public static int trapRainWater(int[][] heightMap) {
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        int[] dirs = {-1, 0, 1, 0, -1};
+        int maxHeight = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                maxHeight = Math.max(maxHeight, heightMap[i][j]);
+            }
+        }
+        int[][] water = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                water[i][j] = maxHeight;
+            }
+        }
+        Queue<int[]> qu = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; i < n; j++) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    if (water[i][j] > heightMap[i][j]) {
+                        water[i][j] = heightMap[i][j];
+                        qu.offer(new int[]{i, j});
+                    }
+                }
+            }
+        }
+
+        while (!qu.isEmpty()) {
+            int[] curr = qu.poll();
+            int x = curr[0];
+            int y = curr[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dirs[i], ny = y + dirs[i + 1];
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
+                    continue;
+                }
+                if (water[x][y] < water[nx][ny] && water[nx][ny] > heightMap[nx][ny]) {
+                    water[nx][ny] = Math.max(water[x][y], heightMap[nx][ny]);
+                    qu.offer(new int[]{nx, ny});
+
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res += water[i][j] - heightMap[i][j];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
