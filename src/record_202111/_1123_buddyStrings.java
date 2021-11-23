e1641d1c7b4a43ec913bf340e91f079ccff83bab
zhoubao <zhoubao_z@163.com>
2021-11-23 15:35:43 +0800

    commit 1123

---
+++ src/record_202111/_1123_buddyStrings.java
@@ -0,0 +1,30 @@
+package record_202111;
+
+/**
+ * https://leetcode-cn.com/problems/buddy-strings/
+ */
+
+public class _1123_buddyStrings {
+
+    public static boolean buddyStrings(String s, String goal) {
+        int n = s.length(), m = goal.length();
+        if (n != m) return false;
+        int[] cnt1 = new int[26], cnt2 = new int[26];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int a = s.charAt(i) - 'a', b = goal.charAt(i) - 'a';
+            cnt1[a]++; cnt2[b]++;
+            if (a != b) sum++;
+        }
+        boolean ok = false;
+        for (int i = 0; i < 26; i++) {
+            if (cnt1[i] != cnt2[i]) return false;
+            if (cnt1[i] > 1) ok = true;
+        }
+        return sum == 2 || (sum == 0 && ok);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(buddyStrings("abcd","badc"));
+    }
+}
